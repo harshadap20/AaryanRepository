77bf63f4e39234465597d225ab501324d983cdea
punde <punde@LAPTOP-QPKB39ML.ht.home>
2024-11-27 19:43:29 -0500

    pls commit

---
+++ AaryanTest/src/aaryanTestPack/ExpressionLearn.java
@@ -0,0 +1,19 @@
+package aaryanTestPack;
+
+public class ExpressionLearn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b;
+		b = (2+(-5))!=3;
+		System.out.println(b);
+		//b = 'm' < = 'p';
+		//System.out.println(b);
+		b = '*' < '*';
+		System.out.println(b);
+		
+		System.out.println(b);
+
+	}
+
+}
